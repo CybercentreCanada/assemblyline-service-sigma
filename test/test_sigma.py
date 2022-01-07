a9a993915a35b0a08e2e1a883b5420bc8f3878ac
cccs-kevin <59843993+cccs-kevin@users.noreply.github.com>
2022-01-07 08:42:54 -0500

    Adding unit tests

---
+++ test/test_sigma.py
@@ -0,0 +1,48 @@
+import pytest
+
+
+class TestEventDataSection:
+    @staticmethod
+    @pytest.mark.parametrize("event, expected_tags",
+        [
+            ({
+                'Event': {
+                    'EventData': {
+                        "CommandLine": "blah blah blah http://evil.com/bad blah blah"
+                    },
+                    'System': {
+                        'Channel': 'Microsoft-Windows-Sysmon/Operational',
+                        'EventID': 13,
+                    }
+                }
+            }, {"network.dynamic.uri": ["http://evil.com/bad"]}),
+            ({
+                 'Event': {
+                     'EventData': {
+                         "ParentCommandLine": "ftp://good.com/justkidding blah blah http://evil.com/bad blah blah"
+                     },
+                     'System': {
+                         'Channel': 'Microsoft-Windows-Sysmon/Operational',
+                         'EventID': 13,
+                     }
+                 }
+             }, {"network.dynamic.uri": ["ftp://good.com/justkidding", "http://evil.com/bad"]}),
+            ({
+                 'EventData': {
+                     'Data': [{
+                         "@Name": "CommandLine",
+                         "#text": "ftp://good.com/justkidding blah blah http://evil.com/bad blah blah"
+                     }]
+                 },
+                 'System': {
+                     'Channel': 'Microsoft-Windows-Sysmon/Operational',
+                     'EventID': 13,
+                 }
+             }, {"network.dynamic.uri": ["ftp://good.com/justkidding", "http://evil.com/bad"]}),
+    ])
+    def test_init(event, expected_tags):
+        from sigma_.sigma import EventDataSection
+        from assemblyline_v4_service.common.balbuzard.patterns import PatternMatch
+        patterns = PatternMatch()
+        actual_res_sec = EventDataSection(event, patterns.PAT_URI_NO_PROTOCOL)
+        assert set(actual_res_sec.tags["network.dynamic.uri"]) == set(expected_tags["network.dynamic.uri"])
