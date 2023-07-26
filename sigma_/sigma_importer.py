1015ceee2438d057bcb1940de9bebef99a4b35a9
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-07-26 14:24:38 +0000

    Remove batching from updater before `add_update_many` call

--- sigma_/sigma_importer.py
+++ sigma_/sigma_importer.py
@@ -7,8 +7,6 @@ from assemblyline.common import forge
 from assemblyline.odm.models.signature import Signature
 
 UPDATE_CONFIGURATION_PATH = os.environ.get('UPDATE_CONFIGURATION_PATH', None)
-BATCH_SIZE_LIMIT = int(os.environ.get('SIG_BATCH_SIZE', 1000))
-
 
 class SigmaImporter:
     def __init__(self, al_client, logger=None):
@@ -26,7 +24,6 @@ class SigmaImporter:
     def _save_signatures(self, files: List[str], source: str, default_classification: str = None):
         upload_list = []
         order = 1
-        order_completed = 0
         add_update_many = self.update_client.signature.add_update_many
         for file in files:
             signature_string = open(file, 'r').read()
@@ -54,13 +51,7 @@ class SigmaImporter:
             upload_list.append(sig.as_primitives())
             order += 1
 
-            # If we hit the batch size limit, send to API
-            if order % BATCH_SIZE_LIMIT == 0:
-                self.log.info(f'Batch limit reached: {BATCH_SIZE_LIMIT}. Sending batch to Signature API..')
-                order_completed += add_update_many(source, 'sigma', upload_list, dedup_name=False)['success']
-                upload_list = []
-
-        order_completed += add_update_many(source, 'sigma', upload_list, dedup_name=False)['success']
+        order_completed = add_update_many(source, 'sigma', upload_list, dedup_name=False)['success']
         self.log.info(f"Imported {order_completed}/{order - 1} signatures from {source} into Assemblyline")
 
         return order_completed
